deaa0240695cf92df3c046a7eb73b9d1fb2724f7
Mamaev_Andrei <mamaev_andrei@mail.ru>
2014-04-30 13:45:29 +0700

    TEST5

--- Desktop/GIT/script.js
+++ Desktop/GIT/script.js
@@ -1,37 +1,48 @@
-(function($){
-  $(document).ready(function(){
-    
-	
-	$("#clickme").click(function(){
-		if (!$("#footer").hasClass("hidden")) {
-			$("#footer").hide("slow"); 
-			$("#footer").addClass("hidden");
-		}
-		else {
-			$("#footer").show("slow");
-			$("#footer").removeClass("hidden");
-		}
-	});
-$("#block-system-main").mouseenter(function() {
-    $( this ).css({
-      "font-size": 25,
-      "font-weight": "bolder"
-    });
-  });
-  $("#block-system-main").mouseleave(function() {
-    $( this ).css({
-      "font-size": 16,
-      "font-weight": "normal"
-    });
-  });
+(function($) {
+    $(document).ready(function() {
 
-   $("#clickone").click(function(){
-       $("#block-views-portfel-block .views-row:last-child").remove();
+
+        $("#clickme").click(function() {
+            if (!$("#footer").hasClass("hidden")) {
+                $("#footer").hide("slow");
+                $("#footer").addClass("hidden");
+            }
+            else {
+                $("#footer").show("slow");
+                $("#footer").removeClass("hidden");
+            }
+        });
+        $("#block-system-main").mouseenter(function() {
+            $(this).css({
+                "font-size": 25,
+                "font-weight": "bolder"
+            });
+        });
+        $("#block-system-main").mouseleave(function() {
+            $(this).css({
+                "font-size": 16,
+                "font-weight": "normal"
+            });
+        });
+       var s = 0;
        
-   });
-      
-  
+        $("#clickone").click(function() {
+            var s = 0;
+            $("#block-views-portfel-block .views-row:last-child").remove();
+            $("#block-views-portfel-block .views-row").each(function(){
+             s = s + 1;
+             
 
-	
-  });    
+            });
+       
+        alert(s);
+         
+       
+        });
+        
+
+
+
+
+    });
 })(jQuery); 
