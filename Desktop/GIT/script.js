8479c2a2de7ce64edffede5aef6fb23d6d99b55b
Mamaev_Andrei <mamaev_andrei@mail.ru>
2014-05-05 13:30:38 +0700

    TEST11

---
+++ Desktop/GIT/script.js
@@ -0,0 +1,40 @@
+(function($) {
+    $(document).ready(function() {
+        $("#clickme").click(function() {
+            if (!$("#footer").hasClass("hidden")) {
+                $("#footer").hide("slow");
+                $("#footer").addClass("hidden");
+            }
+            else {
+                $("#footer").show("slow");
+                $("#footer").removeClass("hidden");
+            }
+        });
+        $("#block-system-main").mouseenter(function() {
+            $(this).css({
+                "font-size": 25,
+                "font-weight": "bolder"
+            });
+        });
+        $("#block-system-main").mouseleave(function() {
+            $(this).css({
+                "font-size": 16,
+                "font-weight": "normal"
+            });
+        });
+function classNumber(a) {
+    return a + 1;
+}
+            var click_number = 0;
+            $("#clickone").click(function() {
+            click_number = click_number + 1;
+            var checkklass = 0;
+            $("#block-views-portfel-block .views-row:last-child").remove();
+            $("#block-views-portfel-block .views-row").each(function(){
+             checkklass = classNumber(checkklass);
+            });
+             alert(checkklass);
+             alert(click_number);
+            });
+    });
+})(jQuery); 
